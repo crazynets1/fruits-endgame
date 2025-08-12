a617b107a3684488e402cb75e02f2e31d166e64f
Kassim Amir <amir.kassim946@gmail.com>
2025-08-12 11:36:45 +0300

    commit the initial files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
+import React from "react"
+import { clsx } from 'clsx'
 import './App.css';
+import languages from './fruits';
 
 function App() {
+  //state values
+  const [currentWord] = React.useState('react')
+  const [guessedLetters, setGuessedLetters] = React.useState([])
+
+  //derived values
+  const wrongGuessCount = guessedLetters.filter(letter => !currentWord.includes(letter)).length
+  const isGameWon = currentWord.split('').every(letter => guessedLetters.includes(letter))
+  const isGameLost = wrongGuessCount >= languages.length - 1
+  const isGameOver = isGameWon || isGameLost
+
+  //static values
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+  function addGuessed(letter, index) {
+    setGuessedLetters(oldGuessedLetters => {
+      return guessedLetters.includes(letter) ? oldGuessedLetters : [...oldGuessedLetters, letter]
+    })}
+
+  const langElements = languages.map((lang, index) => {
+    const isLangLost = index < wrongGuessCount
+    const myStyle = clsx('chip', {lost : isLangLost})
+    const styles ={
+      backgroundColor: lang.backgroundColor,
+      color:lang.color
+    }
+    return <span className={myStyle} key={lang.name} style={styles}>{lang.name}</span>
+  })
+
+  const wordElements = currentWord.split('').map(letter => {
+    return <span>{guessedLetters.includes(letter) ? letter.toUpperCase() : ''}</span>
+  })
+  
+  const keyboardElements = 
+        alphabet.split('').map(letter => {
+          const isGuessed = guessedLetters.includes(letter)
+          const isCorrect = isGuessed && currentWord.includes(letter)
+          const isWrong = isGuessed && !currentWord.includes(letter)
+          const styles = clsx({
+            correct: isCorrect,
+            wrong: isWrong
+          })
+
+          return <button onClick={() => addGuessed(letter)} key={letter.name} className={styles}>
+                  {letter.toUpperCase()}
+                </button>
+        })
+  function renderStatus() {
+    if(!isGameOver) {
+      return null
+    }
+    if(isGameWon) {
+      return(
+        <>
+          <h2>You Win!</h2>
+          <p>Well done! 🎉</p>
+        </> 
+      )
+    }else {
+      return (
+        <>
+          <h2>You Lost!</h2>
+          <p>You better start learning Assembly!</p>
+        </>  
+      )
+    }
+  }
+
+  const className = clsx('status-message', {
+    won: isGameWon,
+    lost:isGameLost
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <section className='header'>
+      <h1>Fruits: Endgame</h1>
+      <p>
+        Guess the word in under 8 attempts to keep the programming 
+        world safe from Assembly!
+      </p>
+    </section>
+    <section className={className}>
+      {renderStatus()}
+    </section>
+    <section className='languages-section'>
+      {langElements}
+    </section>
+    <section className='word'>
+      {wordElements}
+    </section>
+    <section className='keyboard'>
+      {keyboardElements}
+    </section>
+    {isGameOver && <button className='newgame-btn'>New Game</button>}
+    </main>
+  )
 }
 
 export default App;
